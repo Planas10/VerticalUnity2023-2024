126937cd39b4f0aa9e8cd97e5f170faaf9c7b849
PCPORTATILPREST\Marc <marc.planas@enti.cat>
2023-12-04 18:15:55 +0100

    04/12/2023

--- Assets/Scripts/CameraS.cs
+++ Assets/Scripts/CameraS.cs
@@ -4,7 +4,16 @@ using UnityEngine;
 
 public class CameraS : MonoBehaviour
 {
-    private void Update()
+    private float rotationX;
+    private float rotationY;
+    private void LateUpdate()
     {
+        rotationX += Input.GetAxis("Mouse Y");
+        rotationY += Input.GetAxis("Mouse X");
+
+        rotationX = Mathf.Clamp(rotationX, -40f, 40f);
+
+        transform.eulerAngles = new Vector3(rotationX, rotationY, 0);
     }
+
 }
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,10 +5,30 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    //Script references
+    [SerializeField] private GameManager gameManager;
+
+    //Component references
+    private CharacterController characterController;
+
+    //This script variables
     [SerializeField] private float speed;
+
+    private void Awake()
+    {
+        characterController = GetComponent<CharacterController>();
+        Cursor.lockState = CursorLockMode.Locked;
+    }
 
     private void Update()
     {
-        
+        Movement();
     }
+
+    private void Movement() {
+
+        Vector3 MoveDirection = new Vector3(Input.GetAxis("Horizontal"), -gameManager.Gravity, Input.GetAxis("Vertical"));
+        characterController.Move(MoveDirection * speed * Time.deltaTime);
+    }
+
 }
